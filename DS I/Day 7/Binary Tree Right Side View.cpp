b0f4127f8ead166f4c106c4078d36db8af360fff
vikas-1923 <83959413+vikas-1923@users.noreply.github.com>
2022-07-11 12:39:24 +0530

    Update Binary Tree Right Side View.cpp

--- DS I/Day 7/Binary Tree Right Side View.cpp
+++ DS I/Day 7/Binary Tree Right Side View.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    void recur(TreeNode*root,int level,vector<int>&ans)
+    {
+        if(root==NULL)
+            return;
+        if(level==ans.size())
+            ans.push_back(root->val);
+            
+        recur(root->right,level+1,ans);
+        recur(root->left,level+1,ans);
+    }
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int>ans;
+        recur(root,0,ans);
+        return ans;
+        
+    }
+};
