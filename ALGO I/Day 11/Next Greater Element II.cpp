1262eaf35a7196db39f8aced30c013bc3191b43a
vikas-1923 <83959413+vikas-1923@users.noreply.github.com>
2022-08-05 03:46:03 +0530

    Create Next Greater Element II.cpp

---
+++ ALGO I/Day 11/Next Greater Element II.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n=nums.size();
+         
+        vector<int>ans(n,-1);
+        stack<int>s;
+        for(int i=2*n-1;i>=0;i--)
+        {
+            while(!s.empty() && s.top()<=nums[i%n])
+            {
+            s.pop();
+            }
+            if(i<n)
+            {
+                if(!s.empty())
+                {
+                    ans[i]=s.top();
+                }
+            }
+            s.push(nums[i%n]);
+        }
+        return ans;
+        
+    }
+};
