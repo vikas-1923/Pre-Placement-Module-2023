b123ab7acdc121afcca7120ae913fd403d8e1556
vikas-1923 <83959413+vikas-1923@users.noreply.github.com>
2022-08-05 03:23:47 +0530

    Create Diameter of Binary Tree.cpp

---
+++ DS I/Day 8/Diameter of Binary Tree.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+   int maxx = INT_MIN ;
+    int height(TreeNode *root)
+    {
+        if(!root)
+            return 0;
+        
+        int lh = height(root->left);
+        int rh = height(root->right);
+        
+        maxx = max(maxx,lh+rh);
+        
+        return 1+max(lh,rh);
+    }
+    int diameterOfBinaryTree(TreeNode* root) 
+    {
+        height(root);
+        return maxx;
+        
+    }
+};
