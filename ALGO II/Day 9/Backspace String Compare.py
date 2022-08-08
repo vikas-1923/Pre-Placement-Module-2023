667ffe13b66f1d67e3aaba8a38b62b865334f641
vikas-1923 <83959413+vikas-1923@users.noreply.github.com>
2022-08-08 17:37:46 +0530

    Create Backspace String Compare.py

---
+++ ALGO II/Day 9/Backspace String Compare.py
@@ -0,0 +1,12 @@
+class Solution:
+    def backspaceCompare(self, s, t):
+        return self.parse(s) == self.parse(t)
+    
+    def parse(self, x):
+            res = []
+            for c in x:
+                if c != "#":
+                    res.append(c)
+                else:
+                    if res: res.pop()
+            return res
